ee1f898af7feeb43fd166077dd6274d3d605e51d
Bagus iMac <bagus.stimata@gmail.com>
2020-10-28 09:30:52 +0700

    chaching image strategy 2

--- app/src/main/java/com/example/rest/AppConfig.java
+++ app/src/main/java/com/example/rest/AppConfig.java
@@ -1,7 +1,7 @@
 package com.example.rest;
 
 public class AppConfig {
-    public static final String BASE_URL = "http://192.168.1.105:8085/";
+    public static final String BASE_URL = "http://192.168.1.100:8085/";
     public static String BASIC_AUTH_USERNAME = "admin";
     public static String BASIC_AUTH_PASSWORD = "admin";
 
--- app/src/main/java/com/example/rest/PhotoGalleryActivity.java
+++ app/src/main/java/com/example/rest/PhotoGalleryActivity.java
@@ -1,19 +1,27 @@
 package com.example.rest;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Base64;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.load.model.LazyHeaders;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.example.rest.ui.GlideImageLoader;
 
 import butterknife.BindView;
@@ -31,6 +39,9 @@ public class PhotoGalleryActivity extends AppCompatActivity {
     ImageView imageView3;
     @BindView(R.id.imageView4)
     ImageView imageView4;
+
+    @BindView(R.id.pbLoading1)
+    ProgressBar pbLoading1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,26 +73,64 @@ public class PhotoGalleryActivity extends AppCompatActivity {
          * Glide Caching Srategy
          * https://android.jlelse.eu/best-strategy-to-load-images-using-glide-image-loading-library-for-android-e2b6ba9f75b2
          */
+        CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(this);
+        circularProgressDrawable.setStrokeWidth(5f);
+        circularProgressDrawable.setCenterRadius(30f);
+        circularProgressDrawable.start();
+
         RequestOptions requestOptions = RequestOptions
-                .diskCacheStrategyOf(DiskCacheStrategy.AUTOMATIC)
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC);
+                .placeholderOf(circularProgressDrawable)
+                .error(circularProgressDrawable)
+                .skipMemoryCacheOf(true)
+                .diskCacheStrategyOf(DiskCacheStrategy.NONE)
+                .diskCacheStrategy(DiskCacheStrategy.NONE);
+
 
         Glide.with(this)
                 .load(glideUrl)
                 .circleCrop()
+                .placeholder(circularProgressDrawable)
+                .error(circularProgressDrawable)
                 .apply(requestOptions)
+//                .diskCacheStrategy(DiskCacheStrategy.NONE)
+//                .skipMemoryCache(true)
                 .apply(new RequestOptions().override(300, 300))
                 .into(imageView1);
         Glide.with(this)
                 .load(glideUrl)
                 .circleCrop()
-                .apply(requestOptions)
+//                .placeholder(circularProgressDrawable)
+//                .error(circularProgressDrawable)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        pbLoading1.setVisibility(View.VISIBLE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        pbLoading1.setVisibility(View.VISIBLE);
+                        return false;
+                    }
+                })
+//                .apply(requestOptions)
+//                .diskCacheStrategy(DiskCacheStrategy.NONE)
+//                .skipMemoryCache(true)
                 .apply(new RequestOptions().override(300, 300))
                 .into(imageView2);
+
+
+
         Glide.with(this)
-                .load(glideUrl)
+//                .load(glideUrl)
+                .load("https://raw.githubusercontent.com/bumptech/glide/master/static/glide_logo.png")
                 .circleCrop()
+                .placeholder(circularProgressDrawable)
+                .error(circularProgressDrawable)
                 .apply(requestOptions)
+//                .diskCacheStrategy(DiskCacheStrategy.NONE)
+//                .skipMemoryCache(true)
                 .apply(new RequestOptions().override(300, 300))
                 .into(imageView3);
 
@@ -104,7 +153,7 @@ public class PhotoGalleryActivity extends AppCompatActivity {
 //                .apply(new RequestOptions().override(300, 300))
 //                .into(imageView4);
 
-        new GlideImageLoader(imageView4, null).load(url, options);
+        new GlideImageLoader(imageView4, pbLoading1).load(url, options);
 
     }
 }
