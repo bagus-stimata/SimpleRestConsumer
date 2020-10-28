f5651d4767a192905faadd9102fae55331ab4103
bagus-stimata <bagus.stimata@gmail.com>
2020-10-28 20:22:42 +0700

    Oke selesai semua untuk 1. Retrofit2: Retrofit lebih baru update terakhir tahun 2020 2. Spring Rest Template adalah lama yaitu 2015

--- app/src/main/java/com/example/rest/config/ApiRetrofitRetrofit.java
+++ app/src/main/java/com/example/rest/config/ApiRetrofitRetrofit.java
@@ -4,6 +4,7 @@ import com.example.rest.AppConfig;
 
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -16,12 +17,28 @@ public class ApiRetrofitRetrofit {
     public static Retrofit getClient() {
         if (retrofit==null) {
             HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+
+            /**
+             * 1. Simple
+             */
+//            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                    .addInterceptor(interceptor.setLevel(HttpLoggingInterceptor.Level.BODY))
+//                    .connectTimeout(2, TimeUnit.MINUTES)
+//                    .writeTimeout(2, TimeUnit.MINUTES)
+//                    .readTimeout(2, TimeUnit.MINUTES)
+//                    .build();
+
+
+            /**
+             * 2. With Changing Method
+             */
             OkHttpClient okHttpClient = new OkHttpClient.Builder()
                     .addInterceptor(interceptor.setLevel(HttpLoggingInterceptor.Level.BODY))
                     .connectTimeout(2, TimeUnit.MINUTES)
                     .writeTimeout(2, TimeUnit.MINUTES)
                     .readTimeout(2, TimeUnit.MINUTES)
                     .build();
+
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(AppConfig.BASE_URL)
